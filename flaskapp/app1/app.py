28bf6b1edb3552846392535cff0154a6bd70cb03
Wordh Ul Hasan <wordh.u.hasan@ieee.org>
2021-06-08 12:58:53 -0500

    Table report moved to new tab. Old tabs removed

--- flaskapp/app1/app.py
+++ flaskapp/app1/app.py
@@ -95,56 +95,78 @@ def init_dashboard(server):
                         className='nine columns div-for-charts bg-grey',
                         children=[
                             dcc.Tabs([
-                                dcc.Tab(
-                                    label='Revenue Vehicles Statistics',
-                                    style=tab_style,
-                                    selected_style=tab_selected_style,
-                                    children=[
-                                        html.H2(
-                                            'Small Urban & Rural Transit Systems Revenue Vehicles Fleet Information Reports'),
-                                        html.P(
-                                            'Reports are generated from National Transit Database (NTD) data based on 2019 reporting year, the most current data.',
-                                            style=paragraph_styles),
-                                        html.Br(),
-                                        dcc.Graph(id='revenue-vehicles', config={'displayModeBar': False},
-                                                  animate=True),
-                                        dcc.Graph(id='fleet-composition')
-                                    ]
-                                ),
-                                dcc.Tab(
-                                    label='Revenue Vehicles SGR Reports',
-                                    style=tab_style,
-                                    selected_style=tab_selected_style,
-                                    children=[
-                                        html.H2(
-                                            'Small Urban & Rural Transit Systems Revenue Vehicles State of Good Repair (SGR) Reports'),
-                                        html.P(
-                                            'The revenue vehicles State of Good Repair (SGR) reports are generated from the predicted results of machine learning model developed for small urban and rural transit systems. '
-                                            'The backlog is calculated from the projected retirement years, which were prior to 2021, and the projected vehicle replacements are calculated with the replacement of the same number of retired revenue vehicles. '
-                                            'The backlog and vehicle replacement costs are calculated from the vehicle cost data of the APTA public transportation vehicle database.',
-                                            style=paragraph_styles),
-                                        html.Br(),
-                                        dcc.Graph(id='number-replaced'),
-                                        dcc.Graph(id='replacement-cost')
-                                    ]
-                                ),
-                                dcc.Tab(label='Revenue Vehicles SGR Backlog Reports',
-                                        style=tab_style,
-                                        selected_style=tab_selected_style,
-                                        children=[
-                                            html.H2(
-                                                'Small Urban & Rural Transit Systems Revenue Vehicles SGR Backlog Reports'),
-                                            html.P(
-                                                'The revenue vehicles SGR Backlog are calculated from the projected retirement years, which were prior to 2021, and Backlog Costs are calculated from the APTA’s public transportation vehicle database. '
-                                                'The reports show current backlog and backlog replacement cost by vehicle Type based on NTD data 2019 reporting year.',
-                                                style=paragraph_styles),
-                                            html.Br(),
-                                            dcc.Graph(
-                                                id='backlog-vehicles'),
-                                            dcc.Graph(
-                                                id='backlog-cost-vehicles')
-                                        ]
-                                        ),
+                                # dcc.Tab(
+                                #     label='Revenue Vehicles Statistics',
+                                #     style=tab_style,
+                                #     selected_style=tab_selected_style,
+                                #     children=[
+                                #         html.H2(
+                                #             'Small Urban & Rural Transit Systems Revenue Vehicles Fleet Information Reports'),
+                                #         html.P(
+                                #             'Reports are generated from National Transit Database (NTD) data based on 2019 reporting year, the most current data.',
+                                #             style=paragraph_styles),
+                                #         html.Br(),
+                                #         dcc.Graph(id='revenue-vehicles', config={'displayModeBar': False},
+                                #                   animate=True),
+                                #         dcc.Graph(id='fleet-composition')
+                                #     ]
+                                # ),
+                                # dcc.Tab(
+                                #     label='Revenue Vehicles SGR Reports',
+                                #     style=tab_style,
+                                #     selected_style=tab_selected_style,
+                                #     children=[
+                                #         html.H2(
+                                #             'Small Urban & Rural Transit Systems Revenue Vehicles State of Good Repair (SGR) Reports'),
+                                #         html.P(
+                                #             'The revenue vehicles State of Good Repair (SGR) reports are generated from the predicted results of machine learning model developed for small urban and rural transit systems. '
+                                #             'The backlog is calculated from the projected retirement years, which were prior to 2021, and the projected vehicle replacements are calculated with the replacement of the same number of retired revenue vehicles. '
+                                #             'The backlog and vehicle replacement costs are calculated from the vehicle cost data of the APTA public transportation vehicle database.',
+                                #             style=paragraph_styles),
+                                #         html.Br(),
+                                #         dcc.Graph(id='number-replaced'),
+                                #         dcc.Graph(id='replacement-cost')
+                                #     ]
+                                # ),
+                                # dcc.Tab(label='Revenue Vehicles SGR Backlog Reports',
+                                #         style=tab_style,
+                                #         selected_style=tab_selected_style,
+                                #         children=[
+                                #             html.H2(
+                                #                 'Small Urban & Rural Transit Systems Revenue Vehicles SGR Backlog Reports'),
+                                #             html.P(
+                                #                 'The revenue vehicles SGR Backlog are calculated from the projected retirement years, which were prior to 2021, and Backlog Costs are calculated from the APTA’s public transportation vehicle database. '
+                                #                 'The reports show current backlog and backlog replacement cost by vehicle Type based on NTD data 2019 reporting year.',
+                                #                 style=paragraph_styles),
+                                #             html.Br(),
+                                #             dcc.Graph(
+                                #                 id='backlog-vehicles'),
+                                #             dcc.Graph(
+                                #                 id='backlog-cost-vehicles')
+                                #         ]
+                                #         ),
+                                # dcc.Tab(
+                                #     label=' Report Name ',
+                                #     style=tab_style,
+                                #     selected_style=tab_selected_style,
+                                #     children=[
+                                #         html.H2(
+                                #             'Heading 2'),
+                                #         html.P(
+                                #             'Description',
+                                #             style=paragraph_styles),
+                                #         html.Br(),
+                                #         dcc.Graph(
+                                #             id='vehicle-revenue-miles'),
+                                #         dcc.Graph(
+                                #             id='vehicle-revenue-hours'),
+                                #         dcc.Graph(
+                                #             id='unlinked_passenger_trips'),
+                                #         # html.Div(id='my-output'),
+                                #         dcc.Graph(
+                                #             id='vehicle-revenue-miles-table'),
+                                #     ]
+                                # ),
                                 dcc.Tab(
                                     label=' Report Name ',
                                     style=tab_style,
@@ -162,7 +184,19 @@ def init_dashboard(server):
                                             id='vehicle-revenue-hours'),
                                         dcc.Graph(
                                             id='unlinked_passenger_trips'),
-                                        # html.Div(id='my-output'),
+                                    ]
+                                ),
+                                dcc.Tab(
+                                    label=' Report Name In table format',
+                                    style=tab_style,
+                                    selected_style=tab_selected_style,
+                                    children=[
+                                        html.H2(
+                                            'Heading 2'),
+                                        html.P(
+                                            'Description',
+                                            style=paragraph_styles),
+                                        html.Br(),
                                         dcc.Graph(
                                             id='vehicle-revenue-miles-table'),
                                     ]
