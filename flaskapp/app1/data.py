d59ab414fe0da16db39e4a598196ef30886e5d2c
dilipkmistry <dilipkmistry@gmail.com>
2021-06-24 12:39:05 -0500

    Data converted to csv

--- flaskapp/app1/data.py
+++ flaskapp/app1/data.py
@@ -7,17 +7,17 @@ def init_data():
     data2 = preprocess('data/revenue_vehicle_replacement_cost.csv')
     data3 = preprocess('data/service.csv')
 
-    dataStatforAgenciesRankedByVRM = preprocess('data/Table 34.xlsx')
-    datastatForDemandResponseRankedByVRM = preprocess('data/Table 40.xlsx')
-    datastatForFixedRouteRankedByVRM = preprocess('data/Table 37.xlsx')
+    dataStatforAgenciesRankedByVRM = preprocess('data/Table 34.csv')
+    datastatForDemandResponseRankedByVRM = preprocess('data/Table 40.csv')
+    datastatForFixedRouteRankedByVRM = preprocess('data/Table 37.csv')
 
-    dataStatforAgenciesRankedByVRH = preprocess('data/Table 35.xlsx')
-    datastatForDemandResponseRankedByVRH = preprocess('data/Table 41.xlsx')
-    datastatForFixedRouteRankedByVRH = preprocess('data/Table 38.xlsx')
+    dataStatforAgenciesRankedByVRH = preprocess('data/Table 35.csv')
+    datastatForDemandResponseRankedByVRH = preprocess('data/Table 41.csv')
+    datastatForFixedRouteRankedByVRH = preprocess('data/Table 38.csv')
 
-    dataStatforAgenciesRankedByRidership = preprocess('data/Table 36.xlsx')
-    datastatForDemandResponseRankedByRidership = preprocess('data/Table 42.xlsx')
-    datastatForFixedRouteRankedByRidership = preprocess('data/Table 39.xlsx')
+    dataStatforAgenciesRankedByRidership = preprocess('data/Table 36.csv')
+    datastatForDemandResponseRankedByRidership = preprocess('data/Table 42.csv')
+    datastatForFixedRouteRankedByRidership = preprocess('data/Table 39.csv')
 
     return data1, data2, data3, \
            dataStatforAgenciesRankedByVRM, datastatForDemandResponseRankedByVRM, datastatForFixedRouteRankedByVRM,\
