f7541bb149b7dc37664d86eb2783bd57f84add01
dilipkmistry <dilipkmistry@gmail.com>
2021-06-25 10:05:30 -0500

    Update app.py

--- flaskapp/app1/app.py
+++ flaskapp/app1/app.py
@@ -97,7 +97,7 @@ def init_dashboard(server):
                             dcc.Tabs([
 
                                 dcc.Tab(
-                                    label=' Sevice Report ',
+                                    label=' Services ',
                                     style=tab_style,
                                     selected_style=tab_selected_style,
                                     children=[
@@ -109,7 +109,7 @@ def init_dashboard(server):
                                         html.Br(),
                                         dcc.Tabs([
                                             dcc.Tab(
-                                                label=' Report Name ',
+                                                label=' Transit Services in Chart ',
                                                 style=tab_style,
                                                 selected_style=tab_selected_style,
                                                 children=[
@@ -128,74 +128,7 @@ def init_dashboard(server):
                                                 ]
                                             ),
                                             dcc.Tab(
-                                                label=' Report Name In table format',
-                                                style=tab_style,
-                                                selected_style=tab_selected_style,
-                                                children=[
-                                                    html.H2(
-                                                        'Heading 2'),
-                                                    html.P(
-                                                        'Description',
-                                                        style=paragraph_styles),
-                                                    html.Br(),
-                                                    html.P(
-                                                        'Vehicle Revenue Miles',
-                                                        style=paragraph_styles),
-                                                    dcc.Graph(
-                                                        id='vehicle-revenue-miles-table'),
-                                                    html.Br(),
-                                                    html.Br(),
-                                                    html.P(
-                                                        'Vehicle Revenue Hours',
-                                                        style=paragraph_styles),
-                                                    dcc.Graph(
-                                                        id='vehicle-revenue-hours-table'),
-                                                    html.Br(),
-                                                    html.Br(),
-
-                                                    html.P(
-                                                        'Unlinked Passenger Trips',
-                                                        style=paragraph_styles),
-                                                    dcc.Graph(
-                                                        id='unlinked_passenger_trips-table')
-                                                ]
-                                            ),
-                                        ], style=tabs_styles),
-                                    ]
-                                ),
-                                dcc.Tab(
-                                    label=' Primary Tab 2 ',
-                                    style=tab_style,
-                                    selected_style=tab_selected_style,
-                                    children=[
-                                        html.H2(
-                                            'Heading'),
-                                        html.P(
-                                            'Description',
-                                            style=paragraph_styles),
-                                        html.Br(),
-                                        dcc.Tabs([
-                                            dcc.Tab(
-                                                label=' Report Name 1',
-                                                style=tab_style,
-                                                selected_style=tab_selected_style,
-                                                children=[
-                                                    html.H2(
-                                                        'Heading 2'),
-                                                    html.P(
-                                                        'Description',
-                                                        style=paragraph_styles),
-                                                    html.Br(),
-                                                    dcc.Graph(
-                                                        id='vehicle-revenue-miles'),
-                                                    dcc.Graph(
-                                                        id='vehicle-revenue-hours'),
-                                                    dcc.Graph(
-                                                        id='unlinked_passenger_trips'),
-                                                ]
-                                            ),
-                                            dcc.Tab(
-                                                label=' Report Name 2',
+                                                label=' Transit Services in table format',
                                                 style=tab_style,
                                                 selected_style=tab_selected_style,
                                                 children=[
